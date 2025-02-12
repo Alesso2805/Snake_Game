ecc747dc25e2b64a080f70b87099ca46f094c717
alesso2805 <alessandrodp2222@gmail.com>
2025-02-12 12:42:06 -0500

    updated snake game with counter, food and keys to move the snake around the screen

--- main.py
+++ main.py
@@ -1,41 +1,34 @@
 from turtle import Turtle, Screen
+from snake import Snake
+from scoreboard import Scoreboard
+import time
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Welcome to the Snake Game")
+screen.tracer(0)
 
-# Create a list to store the segments of the snake
-snake_segments = []
-
-# Initialize the snake with three segments
-for i in range(3):
-    segment = Turtle("square")
-    segment.color("white")
-    segment.penup()
-    segment.goto(x=-20*i, y=0)
-    snake_segments.append(segment)
-
-def snake_move():
-    # Move each segment to the position of the previous segment
-    for i in range(len(snake_segments)-1, 0, -1):
-        new_x = snake_segments[i-1].xcor()
-        new_y = snake_segments[i-1].ycor()
-        snake_segments[i].goto(new_x, new_y)
-    # Move the head of the snake forward
-    snake_segments[0].forward(20)
-    screen.ontimer(snake_move, 100)
-
-def snake_left():
-    snake_segments[0].left(90)
-
-def snake_right():
-    snake_segments[0].right(90)
-
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake_move, "Up")
-screen.onkey(snake_left, "Left")
-screen.onkey(snake_right, "Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
 screen.exitonclick()
