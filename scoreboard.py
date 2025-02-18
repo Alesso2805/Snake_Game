9f3df36f92005e03ec39d103bc87ef836583e270
alesso2805 <alessandrodp2222@gmail.com>
2025-02-18 08:58:20 -0500

    added high score function in snake game

--- scoreboard.py
+++ scoreboard.py
@@ -9,17 +9,24 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
+        self.high_score = 0
         self.hideturtle()
         self.goto(0, 260)
         self.update_score()
 
     def update_score(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}" f" High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
